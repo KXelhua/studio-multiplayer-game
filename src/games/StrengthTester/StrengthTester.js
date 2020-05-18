db8e5eda203993ee34965800b905a2aed95dff69
Jazbel <c278wang@uwaterloo.ca>
2020-05-18 18:50:19 -0400

    Win/lose condition with the knot.

--- src/games/StrengthTester/StrengthTester.js
+++ src/games/StrengthTester/StrengthTester.js
@@ -3,6 +3,7 @@ import React from "react";
 import UserApi from "../../UserApi.js";
 import Player from "./Player.js";
 import Rope from "./Rope.js";
+import "./StrengthTester.css";
 export default class StrengthTester extends GameComponent {
   constructor(props) {
     super(props);
@@ -35,25 +36,44 @@ export default class StrengthTester extends GameComponent {
       this.getSessionDatabaseRef().set({ rope: this.state.rope + 1 });
     }
   }
+  rightWidth() {
+    if (this.state.rope >= 0) {
+      return "0%";
+    } else {
+      return this.state.rope * -7 + "%";
+    }
+  }
+
+  leftWidth() {
+    if (this.state.rope <= 0) {
+      return "0%";
+    } else {
+      return this.state.rope * 7 + "%";
+    }
+  }
 
   render() {
-    var id = this.getSessionId();
-    var users = this.getSessionUserIds().map(user_id => (
-      <li key={user_id}>{UserApi.getName(user_id)}</li>
-    ));
     var isHost = this.getSessionCreatorUserId() === this.getMyUserId();
-    var creator = UserApi.getName(this.getSessionCreatorUserId());
-    return (
-      <div>
-        <p>{this.state.rope}</p>
-        <p>{id}</p>
-        <button onClick={() => this.handleButtonClick(isHost)}>
-          Pulling the rope.
-        </button>
-        <Player name={users[0]} points={this.state.rope} />
-        <Player name={users[1]} points={this.state.rope} />
-        <Rope />
-      </div>
-    );
+
+    if (this.state.rope <= -10) {
+      return <div>Host wins, guest lost</div>;
+    } else if (this.state.rope >= 10) {
+      return <div>Guest wins, host lost</div>;
+    } else {
+      /* host and guest are still playing */
+      return (
+        <div id="strengthtester">
+          <p>{this.state.rope}</p>
+          <button onClick={() => this.handleButtonClick(isHost)}>
+            Pulling the rope.
+          </button>
+          <div id="knot-container">
+            <div id="left" style={{ width: this.leftWidth() }} />
+            <img id="knot" src="/games/StrengthTester/knot.png" alt="knot" />
+            <div id="right" style={{ width: this.rightWidth() }} />
+          </div>
+        </div>
+      );
+    }
   }
 }
